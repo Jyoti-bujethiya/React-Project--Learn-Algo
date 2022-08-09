25269d194aa479b66e1485954a3ee2ffdec0a10a
bujethiya-jyoti <jyoti.20194078@mnnit.ac.in>
2022-08-09 16:41:17 +0530

    Updates

---
+++ precache-manifest.7446b068ce204949e489cb84d13bd692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7841162eb79bc2a8707a",
+    "url": "./static/css/main.51aa9c38.chunk.css"
+  },
+  {
+    "revision": "7841162eb79bc2a8707a",
+    "url": "./static/js/main.7841162e.chunk.js"
+  },
+  {
+    "revision": "98763216efe98ab110b9",
+    "url": "./static/css/1.ec1b2585.chunk.css"
+  },
+  {
+    "revision": "98763216efe98ab110b9",
+    "url": "./static/js/1.98763216.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e28ec76fe5331fcfbd173835e8bb1cf2",
+    "url": "./index.html"
+  }
+];
